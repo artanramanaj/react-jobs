97077df27c514239a80cf14302f3b05ee3af11e8
Artan Rramanaj <artan.ramanaj@finnie.consulting>
2025-03-19 12:39:47 +0100

    fixing navigation

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "./Card";
+import { Link } from "react-router-dom";
 function HomeCards() {
   return (
     <>
@@ -15,24 +16,24 @@ function HomeCards() {
               <p className="mt-2 mb-4">
                 Browse our React jobs and start your career today
               </p>
-              <a
-                href="/jobs.html"
+              <Link
+                to="/jobs"
                 className="inline-block rounded-lg bg-black px-4 py-2 text-white hover:bg-gray-700"
               >
                 Browse Jobs
-              </a>
+              </Link>
             </Card>
             <Card className="rounded-lg bg-indigo-100 p-6 shadow-md">
               <h2 className="text-2xl font-bold">For Employers</h2>
               <p className="mt-2 mb-4">
                 List your job to find the perfect developer for the role
               </p>
-              <a
-                href="/add-job.html"
+              <Link
+                to="/add-job"
                 className="inline-block rounded-lg bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-600"
               >
                 Add Job
-              </a>
+              </Link>
             </Card>
           </div>
         </div>
